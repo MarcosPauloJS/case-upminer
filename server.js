9216d635b62c6fba9b304ed2d044dbe969e3f734
MarcosPauloJS <marcospaulooj@gmail.com>
2022-03-17 13:53:48 -0300

    fix router

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
+
+app.use(history({
+  verbose: true
+}))
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 var port = process.env.PORT || 5000
