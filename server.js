97f0c308e3adc42e606c55738f390633b6d4ac5d
MarcosPauloJS <marcospaulooj@gmail.com>
2022-03-17 13:44:42 -0300

    change server.js for deploy

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const serverStatic = require('serve-static')
-const path = require('path')
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-const app = express()
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
-
-const port = process.env.port || 8080
-
+var port = process.env.PORT || 5000
 app.listen(port)
+console.log('server started ' + port)
